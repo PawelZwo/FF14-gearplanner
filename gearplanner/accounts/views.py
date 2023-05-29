54054be7edd0313b5f392a369dfe968d8cfe0430
Paweł Zwoliński <pawel_zwolinski@icloud.com>
2023-05-29 13:48:12 +0200

    singup & login messages, blank links added to index

--- gearplanner/accounts/views.py
+++ gearplanner/accounts/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import *
+from django.contrib import messages
 
 
 class LoginView(View):
@@ -14,7 +15,9 @@ class LoginView(View):
         if form.is_valid():
             user = form.cleaned_data['user']
             if user is not None:
+                username = form.cleaned_data.get('username')
                 login(request, user)
+                messages.success(request, f'{username} logged in!')
             return redirect('index')
         return render(request, 'accounts/form.html', {'form': form})
 
@@ -33,9 +36,11 @@ class AddUser(View):
     def post(self, request):
         form = AddUserForm(request.POST)
         if form.is_valid():
+            username = form.cleaned_data.get('username')
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password1'])
             user.save()
+            messages.success(request, f'Account created for {username}!')
             login(request, user)
             return redirect('index')
         return render(request, 'accounts/form.html', {'form': form})
